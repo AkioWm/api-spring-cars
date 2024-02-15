f3c642fcad3cbd54e9db78d18c790c9b6744aec4
AkioWm <cherif.kante16@gmail.com>
2024-02-15 19:41:53 +0100

    add unit test

---
+++ src/test/java/com/garage/carsapi/CarControllerTest.java
@@ -0,0 +1,33 @@
+package com.garage.carsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CarControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetCars() throws Exception {
+        mockMvc.perform(get("/cars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].makeName", is("Nissan")));
+    }
+
+    @Test
+    public void testOneCar() throws Exception {
+        mockMvc.perform(get("/car/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("modelName", is("GTR")));
+    }
+}
